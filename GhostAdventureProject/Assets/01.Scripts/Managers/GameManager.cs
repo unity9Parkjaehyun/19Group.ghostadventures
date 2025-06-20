fc959112a2f29e36e2670ef36ebee7dee15be298
rlqma1131 <rlqma1131@gmail.com>
2025-06-20 17:38:24 +0900

    feat: Setup

---
+++ GhostAdventureProject/Assets/01.Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    private void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable() => SceneManager.sceneLoaded += OnSceneLoaded;
+    private void OnDisable() => SceneManager.sceneLoaded -= OnSceneLoaded;
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"씬 로드됨: {scene.name}");
+        // 추후 스테이지 초기화, UI 초기화 등 여기에 추가
+    }
+}
