69dd49ddb7c603d74f955d3a6f3847fb77faaa99
hbks0224 <hbks1234@gmail.com>
2025-06-21 02:21:03 +0900

    feat : 기억조각 상호작용 컷신 프로토타입

---
+++ GhostAdventureProject/Assets/test/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(h, v).normalized;
+
+        animator.SetFloat("Speed", moveDirection.magnitude);
+
+        if (h > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); 
+        }
+        else if (h < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ GhostAdventureProject/Assets/test/ob.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class AbsorbObject : MonoBehaviour
+{
+    public GameObject text;
+    public Transform player;
+    private bool isPlayerInTrigger = false;
+    private bool isAbsorbing = false;
+    public Animator animator;
+    public GameObject playerObject;
+    public GameObject topdia;
+    public GameObject chr;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            text.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.F) && !isAbsorbing)
+        {
+            isAbsorbing = true;
+            text.SetActive(false);
+
+            transform.DOMove(player.position, 1.5f);
+            transform.DOScale(Vector3.zero, 1.5f)
+                     .OnComplete(() => Destroy(gameObject));
+
+            animator.SetTrigger("trig");
+            topdia.SetActive(true);
+            chr.SetActive(true);
+            playerObject.SetActive(false);
+        }
+    }
+}
