1636240380a7f8105f5e1353bdd6ec6616fd955b
rlqma1131 <rlqma1131@gmail.com>
2025-06-23 18:51:12 +0900

    feat: 기억조각 생성 및 관리 툴

---
+++ GhostAdventureProject/Assets/01.Scripts/Item/MemoryData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Memory/MemoryData")]
+public class MemoryData : ScriptableObject
+{
+    public enum MemoryType { Positive, Negative, Fake }
+
+    public string memoryID;
+    public MemoryType type;
+
+    public Sprite disguisedSprite;   // 스캔 전 외형
+    public Sprite revealedSprite;    // 스캔 후 외형
+    public Sprite memoryImage;       // 연출용 이미지
+    public string memoryTitle;
+
+    [TextArea(3, 10)]
+    public string memoryDescription;
+}
