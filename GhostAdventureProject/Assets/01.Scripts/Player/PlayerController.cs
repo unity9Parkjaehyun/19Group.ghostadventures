b2ebda6aecb43439f3011bb40f91b07c55e42f6a
DounPark <ehdns1562@gmail.com>
2025-06-23 22:05:26 +0900

    feat 테스트 코드 작성 및 적용 - 빙의 가능

--- GhostAdventureProject/Assets/01.Scripts/Player/PlayerController.cs
+++ GhostAdventureProject/Assets/01.Scripts/Player/PlayerController.cs
@@ -48,4 +48,10 @@ public class PlayerController : MonoBehaviour
     {
         currentTarget = target;
     }
+
+    public void ClearInteractionTarget(IInteractionTarget target)
+    {
+        if (currentTarget == null)
+            currentTarget = null;
+    }
 }
--- GhostAdventureProject/Assets/01.Scripts/Player/QTESystem.cs
+++ GhostAdventureProject/Assets/01.Scripts/Player/QTESystem.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class QTESystem : Singleton<QTESystem>
 {
     private IPossessable currentPossessable;
+    public QTERotatingBar qTERotatingBar;
 
     public void StartQTE(IPossessable target)
     {
         currentPossessable = target;
         Debug.Log("Starting QTE");
+        // qTERotatingBar.StartQTE();
+        
+        // 임시 성공 처리
+        HandleQTEResult(true);
     }
 
-    public void OnQTESuccess()
+    public void HandleQTEResult(bool success)
     {
-        Debug.Log("QTE success");
-    }
-
-    public void OnQTEFail()
-    {
-        Debug.Log("QTE failed");
+        if (success)
+        {
+            Debug.Log("QTE succeeded");
+            // currentPossessable.Possess();
+        }
+        else
+        {
+            Debug.Log("QTE failed");
+            SoulEnergySystem.Instance.Consume(1);
+        }
     }
 }
