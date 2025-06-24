054a293b6ba5057b5ea2e97c0e52906cf54ba0fe
hbks0224 <hbks1234@gmail.com>
2025-06-24 20:18:57 +0900

    feat : timeline 테스트씬

---
+++ GhostAdventureProject/Assets/01.Scripts/MemoryScan/PostProcessController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessController : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    private ColorGrading colorGrading;
+
+    public float Saturation
+    {
+        get
+        {
+            if (volume.profile.TryGetSettings(out colorGrading))
+                return colorGrading.saturation.value;
+            return 0f;
+        }
+        set
+        {
+            if (volume.profile.TryGetSettings(out colorGrading))
+                colorGrading.saturation.value = value;
+        }
+    }
+}
