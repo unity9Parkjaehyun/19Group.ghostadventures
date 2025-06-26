ef4c4d4fe640400c35b0b49f5ff2f5297df6b364
song-ssi <song33@outlook.kr>
2025-06-26 16:23:20 +0900

    Feat: 단서 습득시 인벤토리UI에 표시 / 인벤토리 페이지 넘기는 기능 추가

--- GhostAdventureProject/Assets/01.Scripts/UI/InventorySlot.cs
+++ GhostAdventureProject/Assets/01.Scripts/UI/InventorySlot.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class InventorySlot : MonoBehaviour
 {
     public Image icon;
-    public TextMeshProUGUI clueName;
+    // public TextMeshProUGUI clueName;
 
     public void Setup(ClueData clue)
     {
         icon.sprite = clue.clue_Icon;
-        clueName.text = clue.clue_Name;
+        icon.enabled = true; // 아이콘 표시
+        // clueName.text = clue.clue_Name;
+    }
+
+    internal void Clear()
+    {
+        icon.sprite = null;
+        icon.enabled = false; // 아이콘 숨기기
     }
 }
