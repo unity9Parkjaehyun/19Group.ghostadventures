bc9586eff217081001fb9b7a2aeabe6522550e85
DounPark <ehdns1562@gmail.com>
2025-06-27 15:24:18 +0900

    feat: 에너지 회복 존 refactor: 영혼에너지 시스템

--- GhostAdventureProject/Assets/01.Scripts/Player/SoulEnergySystem.cs
+++ GhostAdventureProject/Assets/01.Scripts/Player/SoulEnergySystem.cs
@@ -7,7 +7,15 @@ public class SoulEnergySystem : Singleton<SoulEnergySystem>
 {
     public int maxEnergy = 10;
     public int currentEnergy;
-
+    
+    public float baseRestoreInterval; // n초마다 자연 회복
+    public int baseRestoreAmount;
+    
+    [HideInInspector] public float currentRestoreInterval;
+    [HideInInspector] public int currentRestoreAmount;
+    
+    private Coroutine passiveRestoreCoroutine;
+    
     private void Start()
     {
         currentEnergy = maxEnergy;
@@ -27,5 +35,41 @@ public class SoulEnergySystem : Singleton<SoulEnergySystem>
         currentEnergy = Mathf.Min(currentEnergy + amount, maxEnergy);
         Debug.Log($"영혼 에너지 {amount} 회복, 현재: {currentEnergy}");
         SoulGauge.Instance.SetSoulGauge(currentEnergy);
+    }
+
+    private void OnEnable()
+    {
+        currentRestoreInterval = baseRestoreInterval;
+        currentRestoreAmount = baseRestoreAmount;
+        passiveRestoreCoroutine = StartCoroutine(PassiveRestoreRoutine());
+    }
+
+    private IEnumerator PassiveRestoreRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(currentRestoreInterval);
+            Restore(currentRestoreAmount);
+        }
+    }
+
+    public void SetRestoreBoost(float newInterval, int newAmount)
+    {
+        currentRestoreInterval = newInterval;
+        currentRestoreAmount = newAmount;
+        
+        if(passiveRestoreCoroutine != null)
+            StopCoroutine(passiveRestoreCoroutine);
+        passiveRestoreCoroutine = StartCoroutine(PassiveRestoreRoutine());
+    }
+
+    public void ResetRestoreBoost()
+    {
+        currentRestoreInterval = baseRestoreInterval;
+        currentRestoreAmount = baseRestoreAmount;
+        
+        if(passiveRestoreCoroutine != null)
+            StopCoroutine(passiveRestoreCoroutine);
+        passiveRestoreCoroutine = StartCoroutine(PassiveRestoreRoutine());
     }
 }
