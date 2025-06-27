197a71c7482240e21f8fa288d10ddae1a7f9827f
DounPark <ehdns1562@gmail.com>
2025-06-27 19:53:52 +0900

    feat: 생일상자

--- GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/Ch1_CelebrityBox.cs
+++ GhostAdventureProject/Assets/01.Scripts/Object/PossessableObject/Ch1_CelebrityBox.cs
@@ -2,7 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ch1_CelebrityBox : BasePossessable
+public class Ch1_CelebrityBox : BasePossible
 {
+    // [SerializeField] private GameObject effect;
+    [SerializeField] private GameObject noteObject;
+    [SerializeField] private Animator animator;
     
+    private bool hasActivated = false;
+
+    protected override void Update()
+    {
+        base.Update();
+        
+        if (!isPossessed || hasActivated)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TriggerBoxEvent();
+        }
+    }
+
+    private void TriggerBoxEvent()
+    {
+        hasActivated = true;
+        
+        // 박스 애니메이션 트리거
+        if(animator != null)
+            animator.SetTrigger("Explode");
+        
+        // 폭발 이펙트 ( 넣는다면 )
+        // if(effect != null)
+        //     Instantiate(effect, transform.position, Quaternion.identity);
+        
+        // note 활성화
+        if(noteObject != null)
+            noteObject.SetActive(true);
+        
+        Unpossess();
+    }
 }
